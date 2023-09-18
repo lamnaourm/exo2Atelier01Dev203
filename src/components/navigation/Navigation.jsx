edd5bc89c599749bc4e1987be1c3a958b2848236
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-18 17:55:30 +0100

    add grid

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -6,15 +6,15 @@ class Navigation extends Component {
     render() { 
         return <nav>
             <ul>
-                <li><a href="Acceuil"></a> </li>
-                <li><a href="Item 1"></a> </li>
-                <li><a href="Item 2"></a> </li>
-                <li><a href="Item 3"></a> </li>
-                <li><a href="Item 4"></a> </li>
-                <li><a href="Item 5"></a> </li>
-                <li><a href="Item 6"></a> </li>
-                <li><a href="Item 7"></a> </li>
-                <li><a href="Item 8"></a> </li>
+                <li><a href="">Acceuil</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
+                <li><a href="">Item 1</a> </li>
             </ul>
         </nav>;
     }
