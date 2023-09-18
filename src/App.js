f7a4955e0ea4d1f0a0e11a8a8e8e98d7d2479895
lamnaourm <mohammed.lamnaour@gmail.com>
2023-09-18 17:32:06 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Content from './components/content/Content';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Navigation />
+      <Content />
+      <Footer />
     </div>
   );
 }
